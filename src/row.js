891bf879f24665bb375dea69d73c6e5732bdcbed
Francisco Novoa Parada <francisco.novoa.14@sansano.usm.cl>
2020-05-12 21:29:07 -0400

    ordenadito

--- src/row.js
+++ src/row.js
@@ -9,22 +9,28 @@ export default function Row(props){
     return(
         <>
 
-        {   props.currentSong===props.i?     
+        {   props.currentSong===props.i? 
+
         <button className="list-group-item list-group-item-action list-group-item-dark bg-dark text-white"
-        onClick={()=>{handleClick()}}
-        >
+        onClick={()=>{handleClick()}}>
+
             <span style={{fontWeight: "bold"}}> {props.i} </span>
             <span style={{fontWeight: "bold"}} >- </span>
             <span >{props.elem.name}</span>
+
         </button>
+
         :
+
         <button className="list-group-item list-group-item-action list-group-item-dark"
-        onClick={()=>{handleClick()}}
-        >
+        onClick={()=>{handleClick()}}>
+
             <span style={{fontWeight: "bold"}}> {props.i} </span>
             <span style={{fontWeight: "bold"}} >- </span>
             <span >{props.elem.name}</span>
+
         </button>
+        
         }
         </>
     )
